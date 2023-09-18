ab8e15b298a15b287f3f9c85f94824c421f1cd1e
Vic Chang <vicchang@12cm.com.tw>
2023-09-18 11:54:27 +0800

    publish practice

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use std::{env, process};
 use minigrep::Config;
 
+
+/// Adds one to the number given.
+///
+/// # Examples
+///
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+///
+/// assert_eq!(6, answer);
+/// ```
+
 fn main() {
-    // let args: Vec<String> = env::args().collect();
     let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
